5fef77a1afabb50ad0b622bf5bcccd1d641ecf0b
Bogdan_Iam <iamnitchi.io.bogdan@student.utcluj.ro>
2023-04-01 17:00:18 +0300

    created a1.c

---
+++ a1/a1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+ 
+
+int main(int argc, char **argv){
+    if(argc >= 2){
+        if(strcmp(argv[1], "variant") == 0){
+            printf("11416\n");
+        }
+    }
+    return 0;
+}
