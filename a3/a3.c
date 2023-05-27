cb1e1a2358e2563f731e063238779720822af059
Bogdan_Iam <iamnitchi.io.bogdan@student.utcluj.ro>
2023-05-27 10:22:45 +0300

    a3 init

---
+++ a3/a3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int main(int argc, char **argv)
+{
+
+    return 0;
+}
